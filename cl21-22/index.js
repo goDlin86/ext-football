38340e16d041581f99ca31b0504153df4218eedc
goDlin <godlin086@gmail.com>
2021-07-14 19:22:55 +0300

    add dates

--- cl21-22/index.js
+++ cl21-22/index.js
@@ -6,8 +6,15 @@ chrome.storage.local.get(['football_api_token'], async ({ football_api_token }) 
         { headers: { 'X-Auth-Token': football_api_token } }
     )
     const data = await resp.json()
+    console.log(data)
+    let matchesByDate = data.matches.map(m => m.utcDate)
+    matchesByDate = [...new Set(matchesByDate)]
+    matchesByDate = matchesByDate.map(date => ({ date, matches: data.matches.filter(m => m.utcDate === date) }))
 
-    for (let index = 0; index < 16; index++) {
-        el.innerHTML += '<div class="group">' + data.matches[index].homeTeam.name + ' - ' + data.matches[index].awayTeam.name + '</div>'
-    }
+    matchesByDate.map(date => {
+        el.innerHTML += '<div class="date">' + date.date + '</div>'
+        date.matches.map(match => {
+            el.innerHTML += '<div class="group">' + match.homeTeam.name + ' - ' + match.awayTeam.name + '</div>'
+        })
+    })
 })
