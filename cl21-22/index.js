211d399a9cdae8ed17b1355dd2419c54400c53f5
goDlin <godlin086@gmail.com>
2021-07-16 10:11:17 +0300

    add react and parcel

--- cl21-22/index.js
+++ cl21-22/index.js
@@ -6,13 +6,14 @@ chrome.storage.local.get(['football_api_token'], async ({ football_api_token }) 
         { headers: { 'X-Auth-Token': football_api_token } }
     )
     const data = await resp.json()
-    console.log(data)
+    //console.log(data)
     let matchesByDate = data.matches.map(m => m.utcDate)
     matchesByDate = [...new Set(matchesByDate)]
     matchesByDate = matchesByDate.map(date => ({ date, matches: data.matches.filter(m => m.utcDate === date) }))
 
     matchesByDate.map(date => {
-        el.innerHTML += '<div class="date">' + date.date + '</div>'
+        const d = new Date(date.date)
+        el.innerHTML += '<div class="date">' + d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate() + '</div>'
         date.matches.map(match => {
             el.innerHTML += '<div class="group">' + match.homeTeam.name + ' - ' + match.awayTeam.name + '</div>'
         })
