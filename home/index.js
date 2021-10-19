3d0ac41aed7ef3923d8068946402092d9a51587c
goDlin <godlin086@gmail.com>
2021-10-19 19:03:55 +0300

    new simple router

--- home/index.js
+++ home/index.js
@@ -1,31 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter, Switch, Route, Link, Redirect } from 'react-router-dom'
+import {
+    goBack,
+    goTo,
+    Link,
+    Router
+  } from 'react-chrome-extension-router'
 import EURO2020 from '../euro2020/index'
 import ChampLeague21 from '../cl21-22/index'
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Link to="/euro20">
-                EURO 2020
-            </Link>
-            <Link to="/cl21">
-                Champions league 2021-2022
-            </Link>
-
-            <Switch>
-                <Route exact path="/euro20">
-                    <EURO2020 />
-                </Route>
-                <Route exact path="/cl21">
-                    <ChampLeague21 />
-                </Route>
-                <Route exact path="/">
-                    <Redirect to="/index.html" />
-                </Route>
-            </Switch>
-        </BrowserRouter>
+        <Router>
+            <div class="menu">
+                <Link component={EURO2020}>
+                    EURO 2020
+                </Link>
+                <Link component={ChampLeague21}>
+                    Champions league 2021-2022
+                </Link>
+            </div>
+        </Router>
     )
 }
 
