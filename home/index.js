0ee9fe6620d567f8940153b21d8fae489da70bf3
goDlin <godlin086@gmail.com>
2022-05-23 20:58:01 +0300

    add ru league template

--- home/index.js
+++ home/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { render } from 'react-dom'
+import { render, createRoot } from 'react-dom'
 import { Link, Router } from 'react-chrome-extension-router'
 import EURO2020 from '../euro2020/index'
 import ChampLeague21 from '../cl21-22/index'
+import RuLeague22 from  '../ru22-23/index'
 
 const App = () => {
     return (
@@ -14,9 +15,12 @@ const App = () => {
                 <Link component={ChampLeague21}>
                     Champions League 2021-2022
                 </Link>
+                <Link component={RuLeague22}>
+                    Чемпионат России 2022-2023
+                </Link>
             </div>
         </Router>
     )
 }
 
-render(<App />, document.getElementById('table'))
+createRoot(document.getElementById('table')).render(<App />)
---
+++ ru22-23/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'
+import BackButton from '../home/BackButton'
+import './style.css'
+
+export default function RuLeague22 () {
+
+    return (
+        <div class="ru-league">
+            <BackButton />
+            <div class="ru-logo"></div>
+            Чемпионат России 2022-2023
+        </div>
+    )
+}
