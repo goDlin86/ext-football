0985e461b7dba82fa404bf4ad5936971e53ad864
goDlin <godlin086@gmail.com>
2022-07-14 17:14:01 +0300

    improve stage view

--- ru22-23/StageView.js
+++ ru22-23/StageView.js
@@ -12,11 +12,11 @@ export default function StageView ({ matches }) {
 
     useEffect(() => {
         setMatches(matches.filter(m => m.stageName === stage).reduce((result, item) => {
-            const i = result.findIndex(r => r.day === item.day && r.time === item.time)
+            const i = result.findIndex(r => r.day === item.day)
             if (i >= 0) {
                 result[i].matches.push(item)
             } else {
-                result.push({ day: item.day, time: item.time, matches: [item] })
+                result.push({ day: item.day, matches: [item] })
             }
             return result
         }, []))
@@ -31,16 +31,21 @@ export default function StageView ({ matches }) {
             </div>
             {filterMatches.map(item => (
                 <>
-                    <div  class="ru-title ru-date">{item.day + ', ' + item.time}</div>
+                    <div  class="ru-title ru-date">{item.day}</div>
                     {item.matches.map(m => (
                         <>
-                            <div>{m.name1}</div>
+                            <div class="ru-leftteam">{m.name1}</div>
                             <div><img src={m.club1} width="30" height="30" /></div>
-                            <div>{m.goal1}</div>
-                            <div>-</div>
-                            <div>{m.goal2}</div>
+                            {m.goal1 === "" && m.goal2 === "" ? 
+                                <div class="ru-time">{m.time}</div> :
+                                <>
+                                    <div>{m.goal1}</div>
+                                    <div>-</div>
+                                    <div>{m.goal2}</div>
+                                </>
+                            }
                             <div><img src={m.club2} width="30" height="30" /></div>
-                            <div>{m.name2}</div>
+                            <div class="ru-rightteam">{m.name2}</div>
                         </>
                     ))}
                 </>
